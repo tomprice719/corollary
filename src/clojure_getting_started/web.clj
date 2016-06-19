978dccdd29dbf7c276c71f399bb877c9e6a667f0
Tom Price <tprice7@gmail.com>
2016-06-18 20:16:06 -0400

    added stuff with bootstrap and selmer

--- src/clojure_getting_started/web.clj
+++ src/clojure_getting_started/web.clj
@@ -1,4 +1,5 @@
 (ns clojure-getting-started.web
+  (:use [selmer.parser])
   (:require [compojure.core :refer [defroutes GET PUT POST DELETE ANY]]
             [compojure.handler :refer [site]]
             [compojure.route :as route]
@@ -6,16 +7,21 @@
             [ring.adapter.jetty :as jetty]
             [environ.core :refer [env]]))
 
+(def posts-2 [{:title "newer first post title" :heading "hellooooooooooo"}
+              {:title "post title 2" :heading "have a nice day today"}
+              {:title "third post" :heading "aaaaaaaaaaaaaaaaaaa"}])
+
+(def index-page-2 (render-file "templates/index.html" {:posts posts-2}))
+
 (defn splash []
   {:status 200
    :headers {"Content-Type" "text/plain"}
    :body "Hello from Heroku"})
 
 (defroutes app
-  (GET "/" []
-       (splash))
-  (ANY "*" []
-       (route/not-found (slurp (io/resource "404.html")))))
+  (GET "/" [] index-page-2)
+  (route/resources "/")
+  (route/not-found "Page not found"))
 
 (defn -main [& [port]]
   (let [port (Integer. (or port (env :port) 5000))]
