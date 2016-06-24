5a76f5b263a931d0d20971eeebec14a1bec352b3
Tom Price <tprice7@gmail.com>
2016-06-24 01:39:24 -0400

    simplified selmer templates with inheritance

--- src/corollary/views.clj
+++ src/corollary/views.clj
@@ -11,7 +11,7 @@
            [(str "select title, heading from posts where did = '" id "'")])))
 
 (defn posts [] (query db
-                  ["select did, title, heading from posts"]))
+                      ["select did, title, heading from posts"]))
 
 (def posts-2 [{:title "newer first post title" :heading "hellooooooooooo"}
               {:title "post title 2" :heading "have a nice day today"}
@@ -19,13 +19,11 @@
 
 (defn recent-posts [name selected-id]
   (do
-    (println "Name: " name)
-    (render-file "templates/recent_posts.html" {:posts (posts) :name name :selected-id selected-id})))
+    (render-file "templates/recent_posts.html" {:posts (posts) :name name :selected-id selected-id :page "recent"})))
 
 (defn selected-post [name post-id]
   (do
-    (println "Name: " name)
-    (render-file "templates/selected_post.html" {:name name :post (post post-id)})))
+    (render-file "templates/selected_post.html" {:name name :post (post post-id) :page "selected"})))
 
 (defn compose-post []
-  (render-file "templates/compose_post.html" {})) ;; Is .html extension appropriate for template files?
+  (render-file "templates/compose_post.html" {})) ;; Is .html extension appropriate for template files? Yes, it is.
