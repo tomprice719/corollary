adb4b5e0fe4240677926d6140a4619f890a89fad
Tom Price <tprice7@gmail.com>
2016-06-26 02:05:33 -0400

    simplistic pandoc integration

--- src/corollary/updates.clj
+++ src/corollary/updates.clj
@@ -1,13 +1,16 @@
 (ns corollary.updates
   (require [clojure.java.jdbc :as jdbc]
-           [environ.core :refer [env]]))
+           [environ.core :refer [env]]
+           [clojure.java.shell :refer [sh]]))
 
 (def db (env :database-url))
 
+(defn pandoc [input]
+  (:out (sh "pandoc" "-f" "markdown-raw_html" :in input))) ;; you MUST escape raw HTML
+
 (defn create-post [postid title heading]
-  (println postid title heading)
   (jdbc/insert! db :posts
                 {:did postid
                  :title title
-                 :heading heading}))
+                 :heading (pandoc heading)}))
 
