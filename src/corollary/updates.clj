808d57743eb6933af9caa16f3a87d0475c63b1e0
Tom Price <tprice7@gmail.com>
2016-06-27 16:30:46 -0400

    use pandoc server

--- src/corollary/updates.clj
+++ src/corollary/updates.clj
@@ -1,15 +1,14 @@
 (ns corollary.updates
   (require [clojure.java.jdbc :as jdbc]
            [environ.core :refer [env]]
-           [clojure.java.shell :refer [sh]]
            [clj-http.client :as client]))
 
 (def db (env :database-url))
 
-(defn pandoc [input]
-  (:out (sh "pandoc" "-f" "markdown-raw_html" "--mathjax" :in input))) ;; you MUST escape raw HTML
+;(defn local-pandoc [input]
+;  (:out (clojure.java.shell/sh "pandoc" "-f" "markdown-raw_html" "--mathjax" :in input))) ;; you MUST escape raw HTML
 
-(defn remote-pandoc [input]
+(defn pandoc [input]
   (:body (client/post (env :pandoc-url)
                       {:body input})))
 
