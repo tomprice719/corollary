cbd09351d2aeea798c5b567c4c0ccbb8b1029b71
Tom Price <tprice7@gmail.com>
2016-06-27 02:26:52 -0400

    added remote-pandoc function

--- src/corollary/updates.clj
+++ src/corollary/updates.clj
@@ -1,12 +1,17 @@
 (ns corollary.updates
   (require [clojure.java.jdbc :as jdbc]
            [environ.core :refer [env]]
-           [clojure.java.shell :refer [sh]]))
+           [clojure.java.shell :refer [sh]]
+           [clj-http.client :as client]))
 
 (def db (env :database-url))
 
 (defn pandoc [input]
   (:out (sh "pandoc" "-f" "markdown-raw_html" "--mathjax" :in input))) ;; you MUST escape raw HTML
+
+(defn remote-pandoc [input]
+  (:body (client/post (env :pandoc-url)
+                      {:body input})))
 
 (defn create-post [postid title heading]
   (jdbc/insert! db :posts
