de9462b2295d38938f82b0ac2013def99e976ae7
Tom Price <tprice7@gmail.com>
2016-06-29 18:57:08 -0400

    rewrote recent-post and selected-post views

--- src/corollary/views.clj
+++ src/corollary/views.clj
@@ -10,33 +10,30 @@
   (let [time-diff (- (utils/now) date)]
     (str (quot time-diff 1000) " seconds ago")))
 
-(defn post [id] ;;Get rid of having to call first
+(defn get-post [id] ;;Get rid of having to call first
   (first
     (query db
            [(str "select title, date, processed_content from posts where id = '" id "'")]
            {:row-fn #(update % :date date-string)})))
 
-(defn posts [] (query db
+(defn get-posts [] (query db
                       ["select title, author, date, id from posts"]
                       {:row-fn #(update % :date date-string)}))
 
-(def posts-2 [{:title "newer first post title" :heading "hellooooooooooo"}
-              {:title "post title 2" :heading "have a nice day today"}
-              {:title "third post" :heading "aaaaaaaaaaaaaaaaaaa"}])
-
-(defn recent-posts [{:keys [name selected]}]
+(defn recent-posts [params]
   {:body
    (render-file
      "templates/recent_posts.html"
-     {:posts (posts)
-      :name name
-      :selected selected
-      :page "recent"})})
+     (merge params
+            {:posts (get-posts)
+             :page "recent"}))})
 
-(defn selected-post [{:keys [name selected]}]
+(defn selected-post [{:keys [selected] :as params}]
   {:body
    (render-file "templates/selected_post.html"
-                {:name name :post (post selected) :selected selected :page "selected"})})
+                (merge params
+                       {:post (get-post selected)
+                        :page "selected"}))})
 
 (defn compose-post [params]
   {:body (render-file "templates/compose_post.html" {})})
