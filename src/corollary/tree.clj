98fddc307a080d87ed41895f23042411b77e01bf
Tom Price <tprice7@gmail.com>
2016-07-08 03:31:46 -0400

    more work on tree

--- src/corollary/tree.clj
+++ src/corollary/tree.clj
@@ -2,18 +2,13 @@
 (ns corollary.tree
   (:require [clojure.pprint :refer [pprint]]))
 
-(declare pos-list)
+(declare add-pos-data)
 
-(defn reducer [tree [pos lst] id]
-  (let [new-lst (pos-list tree pos id)]
-    [(first new-lst) (concat lst new-lst)]))
+;;TODO: encode node-data and last-pos in map rather than vector. Or in a record called "reduction"
 
-;;position: text height, arrow height, bottom, indent
-;;Parent previous sibling -> first child:
-;;
-;;child -> next child:
-;;
-;;
+(defn reducer [[node-data last-pos] id]
+  (let [new-node-data (add-pos-data node-data last-pos id)]
+    [new-node-data (get-in new-node-data [id :pos])]))
 
 (defn new-pos [{last-text-height :text-height
                 last-bottom :bottom
@@ -28,15 +23,17 @@
 
 (defn starting-pos [{:keys [bottom indent]}]
   (hash-map :text-height bottom
-           :bottom (+ bottom 1)
-           :indent (+ indent 1)))
+            :bottom (+ bottom 1)
+            :indent (+ indent 1)))
 
-(defn pos-list [tree last-pos id]
+(defn add-pos-data [node-data last-pos id]
   (let
-    [[bottom-pos lst] (reduce (partial reducer tree)
-                              [(starting-pos last-pos) '()]
-                              (get tree id))]
-    (cons (new-pos last-pos bottom-pos id) lst)))
+    [[new-node-data bottom-pos] (reduce reducer
+                                        [node-data (starting-pos last-pos)]
+                                        (get-in node-data [id :children]))]
+    (assoc-in new-node-data
+              [id :pos]
+              (new-pos last-pos bottom-pos id))))
 
 (def init-pos
   (hash-map :text-height 0
@@ -44,27 +41,42 @@
             :bottom 0
             :indent 0))
 
-(pprint (pos-list test-tree init-pos 1))
+(pprint (add-pos-data test-tree init-pos 1))
 
-(clojure.tools.namespace.repl/refresh)
+(defn node-list [node-data parent]
+  (tree-seq (constantly true)
+            #(get-in node-data [% :children])
+            parent))
+
+;;TODO: make this give you the data that Selmer needs.
+(defn draw-data [pos]
+  pos)
+
+(defn draw-data-list [node-data parent]
+  (for [id (node-list node-data parent)
+        with-pos-data [(add-pos-data node-data init-pos parent)]]
+    (draw-data (get-in with-pos-data [id :pos]))))
+
+(pprint (draw-data-list test-tree 1))
 
 (def test-tree
-  (hash-map 1 [2 3]
-            2 [4 5]
-            3 [6 7]
-            4 [8 9]
-            5 [10 11]
-            6 []
-            7 []
-            8 []
-            9 []
-            10 []
-            11 []))
+  (hash-map 1 {:children [2 3]}
+            2 {:children [4 5]}
+            3 {:children [6 7]}
+            4 {:children [8 9]}
+            5 {:children [10 11]}
+            6 {:children []}
+            7 {:children []}
+            8 {:children []}
+            9 {:children []}
+            10 {:children []}
+            11 {:children []}))
 
-;(defn maketree-iter [[chosen queue]]
-;  (let [[[id parent] priority] (peek queue)]
-;    (vect
-;      (assoc chosen
-;        id []
-;        parent (conj (get chosen parent) id)))
-;    (into (pop queue) (get-children id)))
+(defn maketree-iter [[chosen queue]]
+  (let [[{:keys [id parent] :as node} priority] (peek queue)]
+    (vect
+      (->chosen
+        (assoc id node)
+        (update-in [parent :children]
+                   #(conj % id)))
+      (into (pop queue) (get-children id)))))
