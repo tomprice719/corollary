d95d223b3dc0b131292e04e90e8cc6c17f509768
Tom Price <tprice7@gmail.com>
2016-10-01 15:06:28 -0400

    require password on every page with selected post. Also refactored.

--- src/corollary/queries.clj
+++ src/corollary/queries.clj
@@ -5,6 +5,29 @@
            [environ.core :refer [env]]
            [clj-http.client :as client]))
 
+(def posts-per-page 10)
+
+(defn date-string [date]
+  (let [seconds-diff (quot (- (utils/now) date) 1000)
+        minute 60
+        two-minutes (* minute 2)
+        hour (* minute 60)
+        two-hours (* hour 2)
+        day (* hour 24)
+        two-days (* day 2)
+        week (* day 7)
+        two-weeks (* week 2)]
+    (condp > seconds-diff
+      minute (str seconds-diff " seconds ago")
+      two-minutes "1 minute ago"
+      hour (str (quot seconds-diff 60) " minutes ago")
+      two-hours "1 hour ago"
+      day (str (quot seconds-diff 3600) " hours ago")
+      two-days "1 day ago"
+      week (str (quot seconds-diff 86400) " days ago")
+      two-weeks "1 week ago"
+      (str (quot seconds-diff 604800) " weeks ago"))))
+
 (defn get-parents [id row-fn]
   (query db ["select posts.title, posts.date, posts.id from posts join edges on posts.id = edges.parent_id where edges.child_id = ? order by posts.date desc"
              (Integer. id)]
@@ -36,3 +59,39 @@
 (defn pandoc [input]
   (:body (client/post (env :pandoc-url)
                       {:body input})))
+
+(defn get-projects []
+  (query db ["select name, posts.id as root from projects join posts on posts.project_id = projects.id and posts.root=true"]))
+
+(defn get-post [id] ;;Get rid of having to call first
+  (first
+    (query db
+           ["select title, author, posts.date, raw_content, processed_content, hover_text, projects.id as project, projects.password as password
+            from posts join projects on posts.project_id = projects.id where posts.id = ?"
+            (Integer. id)]
+           {:row-fn #(update % :date date-string)})))
+
+(defn post-href [post-id]
+  (str "/selected?selected=" post-id))
+
+(defn link-map []
+  (query db
+         ["select id, title, hover_text from posts where hover_text is not null"]
+         {:row-fn        #(vector (-> % :id post-href)
+                                  {:content (:hover_text %)
+                                   :title   (:title %)})
+          :result-set-fn #(apply hash-map (flatten %))}))
+
+(defn get-posts [project-id page-num]
+  (query db
+         ["select title, author, date, id from posts where project_id = ? order by date desc limit ? offset ?"
+          (Integer. project-id)
+          posts-per-page
+          (* page-num posts-per-page)]
+         {:row-fn #(update % :date date-string)}))
+
+(defn get-comments [post-id]
+  (query db
+         ["select author, date, processed_content from comments where post_id = ? order by date asc"
+          (Integer. post-id)]
+         {:row-fn #(update % :date date-string)}))
