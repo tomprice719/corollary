32254b247f9a00e0d4991ba1876b22149d00a0cb
Tom Price <tprice7@gmail.com>
2016-10-04 18:26:06 -0400

    fixed typo in add projects sql script

--- resources/migrations/20160927205958-add-projects.up.sql
+++ resources/migrations/20160927205958-add-projects.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE PROJECTS(
   ID SERIAL PRIMARY KEY       NOT NULL,
-  PASSWORD            TEXT    NOT NULL,
+  PASSWORD            TEXT    NOT NULL
 );
 
 alter table posts add column project_id INTEGER REFERENCES projects;
